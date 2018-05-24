67abc34a5b8f04006cfb0f60cb0e9e6964b794a4
janmchan <chanjma@icloud.com>
2018-05-24 16:37:55 +1200

    Update login.js

--- database-connections/ASPNETMembership/login.js
+++ database-connections/ASPNETMembership/login.js
@@ -9,7 +9,8 @@ function login (email, password, callback) {
   }
 
     var request = require('request');
-    var url = configuration.silive + "/API/access/user";
+    var url = configuration.domain + "/API/Access/Patient";
+    console.log(url);
     var post_data = {
       "Username": email,
       "Password" : password
@@ -27,23 +28,25 @@ function login (email, password, callback) {
         }
     };
     // Set up the request
+    
     request(post_options, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
+      if(!response || !response.statusCode || response.statusCode === 401)
+        {
+          console.log("Invalid Credentials (from Login)");
+          console.log("ActivityId: " + id);
+          
+          callback(new Error(email, "Invalid Credentials"));
+          
+        }  
+      else if (!error && response.statusCode === 200) {
           console.log("ActivityId: " + id);
           console.log(response.statusCode);
           console.log(body);
           callback(null, body);
         }
-        else if(response.statusCode === 401)
-        {
-          console.log("ActivityId: " + id);
-          console.log("Invalid Credentials (from Login)");
-          callback(new Error(email, "Invalid Credentials"));
-          console.log(body);
-        }
+        
         else {
           console.log("ActivityId: " + id);
-          console.log(response.statusCode);
           console.log(body);
           console.log("error: " + error);
           callback(new Error("Unknown Error Occured (from Login)"));
